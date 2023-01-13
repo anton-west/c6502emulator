935dc4449a42289a078c88a4c496df2261fb64e3
Anton West <anton.west@netikka.fi>
2023-01-13 20:55:56 +0200

    reset opcodes to clean state

--- opcodes.c
+++ opcodes.c
@@ -8,86 +8,13 @@
 #include "cpu.h"
 
 
-int I_ADC(uint8_t byte, Processor *cpu, Ir *ir) { 
-    switch (byte)
-    {
-    case 0x69:
-        printf("ADC\tIMM\n");
-        //set instruction result struct
-        ir->addr_mode = IMM; ir->bytes = 2; ir->cycles = 2;
-        
-        uint16_t operand_byte = fetch(cpu);
-        uint16_t temp_res = (uint16_t)cpu->acc + (uint16_t)operand_byte + (uint16_t)getFlag('C', cpu);
-
-        setFlag('C', temp_res > 255, cpu);
-        setFlag('Z', (temp_res & 0x00FF) == 0, cpu);
-        setFlag('N', temp_res & 0x0080, cpu);
-        int overflow_flag = ((cpu->acc^temp_res) & !(cpu->acc^operand_byte)) & 0x0080;
-        setFlag('V', overflow_flag, cpu);
-
-        cpu->acc = temp_res & 0x00FF;
-        break;
-        
-    case 0x65: printf("ADC\tZEP \n"); break;
-    case 0x75: printf("ADC\tZEX, X\n"); break;
-    case 0x6D: printf("ADC\tABS\n"); break;
-    case 0x7D: printf("ADC\tABS, X\n"); break;
-    case 0x79: printf("ADC\tABS, Y\n"); break;
-    case 0x61: printf("ADC\t(IND,X)\n"); break;
-    case 0x71: printf("ADC\t(IND),Y\n"); break;
-    default:
-        return -1;
-    }
-    return 0;
-}
-
+int I_ADC(uint8_t byte, Processor *cpu, Ir *ir) { ir;}
 int I_AND(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_ASL(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_BCC(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_BCS(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_BEQ(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
-int I_BIT(uint8_t byte, Processor *cpu, Ir *ir) {
-    switch (byte)
-    {
-    case 0x24:
-        {
-            printf("BIT\tZEP\n");
-            //set instruction result struct
-            ir->addr_mode = ZEP; ir->bytes = 2; ir->cycles = 3;
-            uint8_t zptr_byte = fetch(cpu);
-            uint8_t operand_byte = read(cpu, (uint16_t) zptr_byte);
-            
-            uint8_t temp_res = cpu->acc & operand_byte;
-            cpu->acc = temp_res;
-            
-            setFlag('N', operand_byte & 0x80, cpu);
-            setFlag('V', operand_byte & 0x40, cpu);
-            setFlag('Z', temp_res == 0, cpu);
-        }
-        break;
-    
-    case 0x2C:
-        {
-            printf("BIT\tABS\n");
-            //set instruction result struct
-            ir->addr_mode = ABS; ir->bytes = 3; ir->cycles = 4;
-            uint16_t low_byte = fetch(cpu);
-            uint16_t high_byte = fetch(cpu);
-            uint8_t operand_byte = read(cpu, (high_byte)<<8 | low_byte);
-            
-            uint8_t temp_res = cpu->acc & operand_byte;
-            cpu->acc = temp_res;
-            
-            setFlag('N', operand_byte & 0x80, cpu);
-            setFlag('V', operand_byte & 0x40, cpu);
-            setFlag('Z', temp_res == 0, cpu);
-        }
-        break;
-    default:
-        return -1;
-    }
-    return 0;
-}
+int I_BIT(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_BMI(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_BNE(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_BPL(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
@@ -111,67 +38,7 @@ int I_INY(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_JMP(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_JSR(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_LDA(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
-int I_LDX(uint8_t byte, Processor *cpu, Ir *ir) {
-    switch (byte)
-    {
-    case 0xA2:
-        {
-        ir->opcode_mnemonic="LDX";ir->addr_mode=IMM;ir->bytes=2;ir->cycles=2;
-        uint8_t fetched_byte = fetch(cpu);
-        cpu->x_reg = fetched_byte;
-        setFlag('N', fetched_byte & 0x80, cpu);
-        setFlag('Z', fetched_byte == 0, cpu);
-        }
-        break;
-    case 0xA6:
-        {
-        ir->opcode_mnemonic="LDX";ir->addr_mode=ZEP;ir->bytes=2;ir->cycles=3;
-        uint16_t zp_addr = fetch(cpu);
-        uint8_t fetched_byte = read(cpu, zp_addr & 0x00FF);
-        cpu->x_reg = fetched_byte;
-        setFlag('N', fetched_byte & 0x80, cpu);
-        setFlag('Z', fetched_byte == 0, cpu);
-        }
-        break;
-    case 0xB6:
-        {
-        ir->opcode_mnemonic="LDX";ir->addr_mode=ZEY;ir->bytes=2;ir->cycles=4;
-        uint16_t zp_addr = fetch(cpu);
-        uint8_t fetched_byte = read(cpu, (zp_addr + cpu->y_reg) & 0x00FF);
-        cpu->x_reg = fetched_byte;
-        setFlag('N', fetched_byte & 0x80, cpu);
-        setFlag('Z', fetched_byte == 0, cpu);
-        }
-        break;
-    case 0xAE:
-        {
-        ir->opcode_mnemonic="LDX";ir->addr_mode=ABS;ir->bytes=3;ir->cycles=4;
-        uint16_t l_byte = fetch(cpu);
-        uint16_t h_byte = fetch(cpu);
-        uint8_t fetched_byte = read(cpu, (h_byte << 8) | l_byte);
-        cpu->x_reg = fetched_byte;
-        setFlag('N', fetched_byte & 0x80, cpu);
-        setFlag('Z', fetched_byte == 0, cpu);
-        }
-        break;
-    case 0xBE:
-        {
-        ir->opcode_mnemonic="LDX";ir->addr_mode=ABY;ir->bytes=3;ir->cycles=4;
-        uint16_t l_byte = fetch(cpu);
-        uint16_t h_byte = fetch(cpu);
-        uint16_t addr = ((h_byte << 8) | l_byte) + (uint16_t)cpu->y_reg;
-        if ((addr & 0xFF00) != ((h_byte << 8) & 0xFF00)) {ir->cycles+=1;}
-        uint8_t fetched_byte = read(cpu, addr);
-        cpu->x_reg = fetched_byte;
-        setFlag('N', fetched_byte & 0x80, cpu);
-        setFlag('Z', fetched_byte == 0, cpu);
-        }
-        break;
-    default:
-        return -1;
-    }
-    return 0;
-}
+int I_LDX(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_LDY(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_LSR(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
 int I_NOP(uint8_t byte, Processor *cpu, Ir *ir) { ir; }
